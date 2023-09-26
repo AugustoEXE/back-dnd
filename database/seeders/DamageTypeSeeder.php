5d910bd9495bfa5922422547199e75eb6036ee32
AugustoEXE <augusto.cr2005@gmail.com>
2023-09-26 16:51:51 -0300

    seeders and factories

---
+++ database/seeders/DamageTypeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DamageTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
